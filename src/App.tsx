de5b22456df247c046df123089136e6b5450ee8f
Paulo Vieira <paulo.vieira@acct.global>
2021-04-07 21:05:49 -0300

    Apply modal and style layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard/Index';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen,setIsNewTransactionModalOpen] =useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+  
+
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/api/transactions')
+    .then(response =>response.json)
+    .then(data =>console.log(data))
+  },[])
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <>
+      <Header onOpenNewTrasactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard/>
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+                        <h1>Cadastro</h1>
+                </Modal>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createServer} from 'miragejs';
+
 import {App} from './App';
+import { threadId } from 'node:worker_threads';
+
+createServer({
+  routes(){
+    this.namespace='api';
+
+    // this.get('/transactions',() =>{
+
+     
+    // })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
